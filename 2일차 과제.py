4e01d34ce699d4e83cffe386db238a55d2f12735
kdw34700 <31720494+kdw3470@users.noreply.github.com>
2017-09-20 15:13:33 +0900

    url version

--- 2일차 과제.py
+++ 2일차 과제.py
@@ -1,13 +1,23 @@
-f = open('data.txt', 'r')
+
+import urllib.request
+
+url = "http://www.newsis.com/view/?id=NISX20170915_0000097186&cID=10101&pID=10100"
+
+f = urllib.request.urlopen(url)
+data = f.read().decode('utf-8')
+Start = data.find("니콜라스")
+End = data.find("키우고 있다")
+
 D = {}
+#print(data[Start:End])
 
-
-
-
-for Line in f :
-    for i in Line.split() :
-        if i in D :
-            D[i] += 1
-        else :
-            D[i] = 1
+for i in data[Start:End].split() :
+    #print(Line)
+    if i in D :
+        D[i] += 1
+    else :
+        D[i] = 1
 print(D)
+
+
+
